d13278df2f786108288ba6076b43c38bc403f156
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-11-16 10:25:49 -0800

    created users model

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,20 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     @property
     def password(self):
@@ -25,6 +30,8 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'first_name':self.first_name,
+            'last_name':self.last_name,
             'username': self.username,
             'email': self.email
         }
