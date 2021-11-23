22ef76161961fc95f924eed81ab1f666b4d20d38
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-11-22 23:47:28 -0800

    added footer links to splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,4 +1,4 @@
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import './SplashPage.css'
 import TopNavBar from '../TopNavBar';
@@ -14,6 +14,42 @@ const SplashPage = () => {
     return (
         <div id="splash-cnt">
             <TopNavBar />
+            <div className="spl-txt">
+                <p>Everything in this universe is created twice.</p>
+                <p>First, in our minds.</p>
+                <p>Then, in reality.</p>
+            </div>
+            <div className="spl-txt">
+                <p>May this online journal be a treasure trove </p>
+                <p>of your memories and experiences,</p>
+                <p>and a constant companion in your journey</p>
+                <p>towards growth and happiness.</p>
+            </div>
+            <div className="spl-sig">
+                <p>Created with love, </p>
+                <p id="sig">Lakshmi Priya Prakash</p>
+            </div>
+            <div id="spl-footer-cnt">
+					<Link
+						to={{ pathname: "https://www.linkedin.com/in/lakshmi-priya-prakash/" }}
+						target="_blank"
+					>
+						<i
+							className="fab fa-linkedin spl-footer-icon fa-2x"
+
+						/>
+					</Link>
+
+					<Link
+						to={{ pathname: "https://github.com/LakshmiPriyaPrakash" }}
+						target="_blank"
+					>
+						<i
+							className="fab fa-github spl-footer-icon fa-2x"
+
+						/>
+					</Link>
+				</div>
         </div>
     );
 
